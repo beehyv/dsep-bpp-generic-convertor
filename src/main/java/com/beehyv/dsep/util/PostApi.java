3e408846c37ab16e9a98487015fc9fd472c00272
atish <atish@beehyv.com>
2023-02-26 22:22:16 +0530

    Added post api after select, init and confirm

--- src/main/java/com/beehyv/dsep/util/PostApi.java
+++ src/main/java/com/beehyv/dsep/util/PostApi.java
@@ -1,2 +1,49 @@
-package com.beehyv.dsep.util;public class PostApi {
+package com.beehyv.dsep.util;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PostApi {
+
+    public static String post(String requestBody, String bap_uri ) throws IOException {
+            // Create URL object
+            URL url = new URL(bap_uri);
+
+            // Open connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            // Set request headers
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+
+            // Set request body
+            OutputStream os = conn.getOutputStream();
+            os.write(requestBody.getBytes());
+            os.flush();
+            os.close();
+
+            // Check response code
+            int responseCode = conn.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            // Read response body
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+           return response.toString();
+
+    }
+
+
 }
